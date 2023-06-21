e0dc508474d21ddbebc6332cc936918b3b3ee599
PART00007 <parteel_pw2_040@masai.school>
2023-06-21 19:50:42 +0530

    update form part

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -5,51 +5,22 @@
 <diV>
     
     <h1>About Page</h1>
-    <div class="about">
-    <h1>Form Section</h1>
-    
-    <div id="form">
-        <p>Input Text Form</p>
-        <input v-model="msg" placeholder="Enter your text" type="text"/>
-     <h1>{{msg }}</h1>
-     </diV>
-    <div id="form">
-        <p>Input Text Area Form</p>
-    <textarea v-model="msg2" placeholder="add multiple lines" type="text"></textarea>
-    <h1>{{msg2}}</h1>
-    </div>
-    <div id="form">
-        <p>Input Number Form</p>
-    <input v-model="msg3" placeholder="Enter your mobile number" type="number"/>
-        <h1>{{msg3 }}</h1>
-     </div>
-     <div id="form">
-        <p>Input Email Form</p>
-      <input v-model="msg4" placeholder="Enter your email id" type="email"/>
-        <h1>{{msg4 }}</h1>
-     </div>
-     <div id="form">
-        <p>Input Checkbox Form</p>
-    
-    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
-    <label for="jack">Jack</label>
-    
-    <input type="checkbox" id="john" value="John" v-model="checkedNames">
-    <label for="john">John</label>
-    
-    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
-    <label for="mike">Mike</label>
-    
-     
-    </div>
-           
+    <form>
+        <label for="name">Enter your Name</label>
+        <input placeholder="Enter your name"  id="name" type="text" v-model="form.name"/>
+        <br/>
+        <label for="address">Enter your address</label>
+        <input placeholder="Enter your address"  id="address" type="text" v-model="form.address"/>
+        <br/>
+        <label for="phoneno">Enter your PhoneNo.</label>
+        <input placeholder="Enter your PhoneNo."  id="number" type="number" v-model="form.phnNumber"/>
+        <br/>
+        <label for="email">Enter your Email id</label>
+        <input placeholder="Enter your emailid"  id="emailid" type="email" v-model="form.email"/>
+        <br/>
+        <button v-on:click="login()" type="button">Submit</button>
+    </form>
 
-    
-   
-    
-  
-  
-</div>
 
 </div>
     
@@ -58,20 +29,28 @@
 
 </template>
 <script>
+import { Store } from "vuex";
 import AboutView from "../views/AboutView.vue"
 export default{
     components: AboutView,
     data() {
         return {
             apiData: [],
-            msg: "",
-            msg2: "",
-            msg3: "",
-            msg4: "",
-            msg5: "",
+          form :{
+            name: "",
+            address: "",
+            phnNumbe: "",
+            email: "",
+          }
+          
         };
     },
-    components: { AboutView }
+    methods:{
+        login(){
+            (this.form).push(Store.this.state.userDetail)
+        }
+    }
+   
 }
 </script>
 <style>
